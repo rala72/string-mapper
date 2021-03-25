a6d4ae34c062e67b9f15f18f823eb1f88ea6899a
rala72 <code@rala.io>
2021-03-25 13:43:06 +0100

    replace string with getter in ParentTestClass

--- src/test/java/io/rala/testUtils/model/ParentTestClass.java
+++ src/test/java/io/rala/testUtils/model/ParentTestClass.java
@@ -29,7 +29,7 @@ public class ParentTestClass {
     @Override
     public String toString() {
         return "SuperClass{" +
-            "string='" + string + '\'' +
+            "string='" + getString() + '\'' +
             '}';
     }
 }
